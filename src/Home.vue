bbe57e663cd86d1de56bf837e2b53ac98037c203
Marcelo Kenzo <m.kenzo.takamori@accenture.com>
2020-07-11 19:51:04 -0300

    Carousel change - vue-carousel-3d

--- src/Home.vue
+++ src/Home.vue
@@ -1,15 +1,27 @@
 <template>
     <section class="app-home">
         <div class="container">
-            <div class="slideshow-container">
-                <div v-for="(image, index) in carouselImages" :key="index" class="mySlides fade">
-                    <img :src="image.imgSrc">
+            <div class="home-resources-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div id="carousel-home-resources" class="carousel-home-resources">
+                                <carousel-3d 
+                                            :perspective="30"
+                                            :border="0"
+                                            :width="585"
+                                            :height="370"
+                                            :controls-visible="true"
+                                            :space="500"
+                                            :clickable="true">
+                                    <slide v-for="(image, index) in carouselImages" :key="index" :index="index">
+                                        <img :src="image.imgSrc">
+                                    </slide>
+                                </carousel-3d>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <a class="prev" @click="plusSlides(-1)">&#10094;</a>
-                <a class="next" @click="plusSlides(1)">&#10095;</a>
-            </div>
-            <div class="dots">
-                <span v-for="index in quantityImages" :key="index" class="dot" @click="currentSlide(index)"></span> 
             </div>
         </div>
     </section>
@@ -17,152 +29,31 @@
 
 <script>
     import carouselImages from './assets/carousel_mock'
+    import { Carousel3d, Slide } from 'vue-carousel-3d';
     import { eventBus } from './main';
 
     export default {
+        components: {
+            Carousel3d,
+            Slide
+        },
         created() {
             eventBus.$emit('homeLoaded', true);            
-        },
-        mounted() {
-            this.showSlides(this.slideIndex);
         },
         data() {
             return {
                 carouselImages: carouselImages,
-                slideIndex: 1
+                slideIndex: 1,
+                slides: 3
             }
-        },
-        computed: {
-            quantityImages: function() {
-                return this.carouselImages.length;
-            }
-        },
-        methods: {
-            showSlides: function(n) {
-                var i;
-                var slides = document.getElementsByClassName("mySlides");
-                var dots = document.getElementsByClassName("dot");
-                if (n > slides.length) {this.slideIndex = 1}    
-                if (n < 1) {this.slideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";  
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[this.slideIndex-1].style.display = "block";  
-                dots[this.slideIndex-1].className += " active";
-            },
-            plusSlides: function(n) {
-                this.showSlides(this.slideIndex += n);
-            },
-            currentSlide: function(n) {
-                this.showSlides(this.slideIndex = n);
-            }
-        }    
+        }
     }
 </script>
 <style>
-    /* Slideshow container */
-    .slideshow-container {
-        max-width: 800px;
-        position: relative;
-        margin: auto;
-        overflow: hidden;
+    .carousel-home-resources .left-1 {
+        transform: translateX(-500px) translateZ(-400px) rotateY(-30deg) !important;
     }
-
-    .slideshow-container img {
-        vertical-align: middle;
-        width:100%;
-    }
-
-    .mySlides {
-        max-height: 500px;
-    }
-
-    /* Next & previous buttons */
-    .prev, .next {
-        cursor: pointer;
-        position: absolute;
-        top: 50%;
-        width: auto;
-        padding: 16px;
-        margin-top: -22px;
-        color: white;
-        font-weight: bold;
-        font-size: 18px;
-        transition: 0.6s ease;
-        border-radius: 0 3px 3px 0;
-        user-select: none;
-    }
-
-    /* Position the "next button" to the right */
-    .next {
-        right: 0;
-        border-radius: 3px 0 0 3px;
-    }
-
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover, .next:hover {
-        background-color: rgba(0,0,0,0.8);
-    }
-
-    /* Caption text */
-    .text {
-        color: #f2f2f2;
-        font-size: 15px;
-        padding: 8px 12px;
-        position: absolute;
-        bottom: 8px;
-        width: 100%;
-        text-align: center;
-    }
-
-    /* Number text (1/3 etc) */
-    .numbertext {
-        color: #f2f2f2;
-        font-size: 12px;
-        padding: 8px 12px;
-        position: absolute;
-        top: 0;
-    }
-
-    .dots {
-        text-align: center;
-        margin-top: 15px;
-    }
-
-    /* The dots/bullets/indicators */
-    .dot {
-        cursor: pointer;
-        height: 15px;
-        width: 15px;
-        margin: 0 2px;
-        background-color: #bbb;
-        border-radius: 50%;
-        display: inline-block;
-        transition: background-color 0.6s ease;
-    }
-
-    .active, .dot:hover {
-        background-color: #717171;
-    }
-
-    /* Fading animation */
-    .fade {
-        -webkit-animation-name: fade;
-        -webkit-animation-duration: 1.5s;
-        animation-name: fade;
-        animation-duration: 1.5s;
-    }
-
-    @-webkit-keyframes fade {
-        from {opacity: .4} 
-        to {opacity: 1}
-    }
-
-    @keyframes fade {
-        from {opacity: .4} 
-        to {opacity: 1}
+    .carousel-home-resources .right-1 {
+        transform: translateX(500px) translateZ(-400px) rotateY(30deg) !important;
     }
 </style>
